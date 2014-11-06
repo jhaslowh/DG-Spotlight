169461591a79b469be9fb624ba8441b0a4e4ffe4
Jonathan Haslow-Hall <jhaslowh@gmail.com>
2014-11-06 00:35:55 -0500

    Fixed eye vector

--- DG_Spotlight/Build/shader_vertex.cpp
+++ DG_Spotlight/Build/shader_vertex.cpp
@@ -4,13 +4,13 @@
 in vec4 position;       // Position handle 
 in vec3 aNormal;        // Normal handle 
 in vec2 aTexCoordinate; // Texture coord handle 
+uniform vec3 cam;       // Camera location 
 
 // Matrix's 
 uniform mat4 modelm;  // Model Matrix handle
 uniform mat4 viewm;   // View Matrix handle
 uniform mat4 projm;   // Projection Matrix handle
 uniform mat3 normm;   // Normal Matrix handle
-uniform vec3 cam;     // Camera location 
 
 // Output to fragment
 varying vec2 vTexCoord;  // Texture coord handle that both shaders use 
@@ -30,7 +30,7 @@ void main() {
     // Compute light direction 
     vLightDirec = vec3(lightPos - pos);
     // Compute eye value 
-    vEye = pos.xyz - cam;
+    vEye = cam - pos.xyz;
 
     // Send texture cord 
     vTexCoord = aTexCoordinate; 
